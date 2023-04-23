e57db2d6fbf5c7b124d8a331be42127bfb8ca806
anshu <anshumj321@gmail.com>
2023-04-23 09:54:18 +0530

    added cureent date

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Declare variables
+
+// getting date 
+var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+const spantag= document.querySelector('#current-date');
+spantag.textContent=(day+ "/" + month + "/" + year);
