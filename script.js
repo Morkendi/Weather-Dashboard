8dd76c230fcf54638d7d3fbd19f7bb7fdaf95d86
anshu <anshumj321@gmail.com>
2023-04-23 10:12:59 +0530

    hi

--- script.js
+++ script.js
@@ -6,4 +6,4 @@ var month = currentTime.getMonth() + 1
 var day = currentTime.getDate()
 var year = currentTime.getFullYear()
 const spantag= document.querySelector('#current-date');
-spantag.textContent=(day+ "/" + month + "/" + year);
+spantag.textContent=( day+ "/" + month + "/" + year);
